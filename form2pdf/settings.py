9cea707489a18b6cf13bcd5067f64a43e175145d
Hardik Jain Harsora <71239504+Hardik450@users.noreply.github.com>
2025-06-17 21:40:39 +0530

    Update settings.py

--- form2pdf/settings.py
+++ form2pdf/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ld21b!%bmlmgr_zuhh2b!5o%@3!6golb52ydp*bhjuh*j21@^5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
