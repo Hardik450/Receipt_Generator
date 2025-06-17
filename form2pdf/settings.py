ef3fd0305bd13bf98847054d806c28a63c74399f
Hardik Jain Harsora <71239504+Hardik450@users.noreply.github.com>
2025-06-17 21:20:49 +0530

    Update settings.py

--- form2pdf/settings.py
+++ form2pdf/settings.py
@@ -134,6 +134,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # or your provider
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bd0166002@gmail.com'
-EMAIL_HOST_PASSWORD = 'fdcezkbdavnugedw'  # or use environment variables
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''  # or use environment variables
 
